bc2dbf0e0a20d2c2529d36fde7bd3b74375a9e86
kranthi582 <80945041+kranthi582@users.noreply.github.com>
2021-03-19 15:53:13 +0530

    Add files via upload

---
+++ Vechicle_Speed_Control.ino
@@ -0,0 +1,102 @@
+#include<LiquidCrystal.h>
+int RS=12;
+int EN=11;
+int D4=10;
+int D5=9;
+int D6=8;
+int D7=7;
+LiquidCrystal lcd(RS,EN,D4,D5,D6,D7);
+char data[12],temp[12];
+char school[]="3600A48B1F06", Hospital[]="3600A46E708C" , Park[]="3600A49F4F42";
+int i,j,flag=0;
+int m1=5;
+int m2=6;
+
+void setup()
+{
+ Serial.begin(9600);
+ lcd.begin(16,2);
+ pinMode(m1,OUTPUT);
+ pinMode(m2,OUTPUT);
+ lcd.print("Speed Control");
+ lcd.setCursor(0,1);
+ lcd.print("System");
+ delay(1000);
+}
+
+void loop()
+{
+  if(Serial.available())
+  {
+  char a=Serial.read();
+    data[i]=a;
+    i++;
+  }
+
+  
+  if(i==12)
+  {
+    //int cmp_flag=-1;
+  
+      if ((compare(data,school)==1)|| (compare(data,Hospital)==1) )
+      {
+      Serial.print("Danger Zone");
+      lcd.setCursor(0,0);
+      for(int j=0;j<12;j++)
+        {
+          lcd.print(data[j]);
+        }
+      lcd.setCursor(0,1);
+      lcd.print("Slow Speed");
+      analogWrite(m1,100);
+      analogWrite(m2,0);
+      
+      delay(3000);
+      lcd.setCursor(0,0);
+      lcd.print("Normal Speed");
+      analogWrite(m1,255);
+      analogWrite(m2,0);
+      }
+ 
+  
+   else
+    {
+      lcd.clear();
+      Serial.print("Safe Zone");
+      lcd.setCursor(0,0);
+      lcd.print("Normal Speed");
+      analogWrite(m1,255);
+      analogWrite(m2,0);
+//      for(int j=0;j<12;j++)
+//      {
+//        //Serial.print(data[j]);
+//        lcd.setCursor(0,1);
+//        lcd.print(data[j]);
+//      }
+    }
+ 
+  
+//    else
+//    {
+//      Serial.print("Unknown Id");
+//      lcd.setCursor(0,0);
+//      lcd.print("Unknown Id");
+//    }
+       Serial.println();
+    i=0;
+  }
+}
+
+ 
+int compare( char *a,char *b)
+{
+  for(j=0;j<12;j++)
+   {
+    if(a[j]!=b[j])
+   {
+    return 0;
+   }
+   }
+   return 1;
+}
+
--- rescuerobot_code.ino
+++ rescuerobot_code.ino
@@ -1,17 +1,14 @@
-#include<Servo.h>
-void seed();
-// Declare the Servo pin 
-int servoPin = 9; 
-// Create a servo object 
-Servo Servo1; 
-int m1=2;
-int m2=3;
-int m3=4;
-int m4=5;
-int m5=8; //A
-int m6=9; // B
-int m7=10; //C
-int m8=11; //D
+
+int m1=6;
+int m2=7;
+int m3=8;
+int m4=9;
+
+int trigr=2;
+int echo=3;
+long duration;
+int distance;
+
 char state;
 int flag;
 int k=0;
@@ -19,19 +16,17 @@ int n=1;
 
 void setup() 
 {
- // Servo1.attach(servoPin); 
   Serial.begin(9600);
   pinMode(m1,OUTPUT);
   pinMode(m2,OUTPUT);
   pinMode(m3,OUTPUT);
   pinMode(m4,OUTPUT);
-   pinMode(m5,OUTPUT);
-  pinMode(m6,OUTPUT);
-  pinMode(m7,OUTPUT);
-  pinMode(m8,OUTPUT);
+  pinMode(trigr,OUTPUT);
+  pinMode(echo,INPUT);
+
   Serial.println("PROJECT");
-  //attachInterrupt(0, pin_ISR, CHANGE);
 }
+
 
 void loop() 
 {
@@ -40,89 +35,60 @@ if(Serial.available())
   {
     state=Serial.read();
     Serial.print(state);
-    //flag=0;
-//    Servo1.write(90); 
-//    delay(500); 
-//     Servo1.write(0); 
-//   delay(500); 
-
-  if(state=='f')
-  {
-     k=1;
-    digitalWrite(m1,HIGH);
-    digitalWrite(m2,LOW);
-    digitalWrite(m3,HIGH);
-    digitalWrite(m4,LOW);
-    Serial.println("Motor: Forward");
-  }
-   else if(state=='q')
-   {
-   
-       digitalWrite(m5,LOW);//DOWN
-    digitalWrite(m6,HIGH);
-    delay(1000);
-    digitalWrite(m5,LOW);
-      digitalWrite(m6,LOW);
-      
-    digitalWrite(m7,HIGH); //CLOSE
-    digitalWrite(m8,LOW);
-    delay(500);
-     digitalWrite(m7,LOW);
-    digitalWrite(m8,LOW);
     
-    delay(500);
-    digitalWrite(m5,HIGH); ///UP
-    digitalWrite(m6,LOW);
-    delay(1000);
-    digitalWrite(m5,LOW);
-      digitalWrite(m6,LOW);
-      
-     }
+//  if(state=='f')
+//  {
+//     k=1;
+//    digitalWrite(m1,HIGH);
+//    digitalWrite(m2,LOW);
+//    digitalWrite(m3,HIGH);
+//    digitalWrite(m4,LOW);
+//    Serial.println("Motor: Forward");
+//  }
+//   else if(state=='q')
+//   {
+//   
+//       digitalWrite(m5,LOW);//DOWN
+//    digitalWrite(m6,HIGH);
+//    delay(1000);
+//    digitalWrite(m5,LOW);
+//      digitalWrite(m6,LOW);
+//      
+//    digitalWrite(m7,HIGH); //CLOSE
+//    digitalWrite(m8,LOW);
+//    delay(500);
+//     digitalWrite(m7,LOW);
+//    digitalWrite(m8,LOW);
+//    
+//    delay(500);
+//    digitalWrite(m5,HIGH); ///UP
+//    digitalWrite(m6,LOW);
+//    delay(1000);
+//    digitalWrite(m5,LOW);
+//      digitalWrite(m6,LOW);
+//      
+//     }
 
-   else if(state=='c')
+  if(state=='c')
    {
      arm_close();
    }
-   else if(state=='d')  //up
+   
+   else if(state=='u')  //up
    {
     arm_up();
    }
-   else if(state=='e')  //down
+   
+   else if(state=='d')  //down
    {
     arm_down();
    }
+   
    else if(state=='o')
    {
     arm_open();
    }
-  else if(state=='b')
-  {
-    digitalWrite(m1,LOW);
-    digitalWrite(m2,HIGH);
-    digitalWrite(m3,LOW);
-    digitalWrite(m4,HIGH);
-        Serial.println("Motor: Backward");
-        flag=1;
-  }
   
-  else if(state=='r')
-  {
-    digitalWrite(m1,LOW);
-    digitalWrite(m2,HIGH);
-    digitalWrite(m3,HIGH);
-    digitalWrite(m4,LOW);
-    
-        Serial.println("Motor: Right-Turn");
-  }
-
-  else if(state=='l')
-  {
-    digitalWrite(m1,HIGH);
-    digitalWrite(m2,LOW);
-    digitalWrite(m3,LOW);
-    digitalWrite(m4,HIGH);
-    Serial.println("Motor: Left Turn");
-  }
   
   else if(state=='s')
   {
@@ -130,51 +96,69 @@ if(Serial.available())
     digitalWrite(m2,LOW);
     digitalWrite(m3,LOW);
     digitalWrite(m4,LOW);
-      digitalWrite(m5,LOW);
-    digitalWrite(m6,LOW);
-    digitalWrite(m7,LOW);
-    digitalWrite(m8,LOW);
   Serial.println("Motor: OFF");
     }
-
+    
+ else if(state=='t')
+ {
+  distance_val();
+ }
 }
 }
 
 
 void  arm_close()
 {
- digitalWrite(m7,HIGH);
-    digitalWrite(m8,LOW);
+  Serial.println("Close ARM");
+ digitalWrite(m1,HIGH);
+    digitalWrite(m2,LOW);
     delay(500);
-     digitalWrite(m7,LOW);
-    digitalWrite(m8,LOW); 
+     digitalWrite(m1,LOW);
+    digitalWrite(m2,LOW); 
 }
 
 void arm_open()
 
  {
-     digitalWrite(m7,LOW);
-    digitalWrite(m8,HIGH);
+    Serial.println("Open ARM");
+     digitalWrite(m1,LOW);
+    digitalWrite(m2,HIGH);
     delay(500);
-     digitalWrite(m7,LOW);
-    digitalWrite(m8,LOW);
+     digitalWrite(m1,LOW);
+    digitalWrite(m2,LOW);
    }
 
 void arm_up()
 {
-   digitalWrite(m5,HIGH); ///UP
-    digitalWrite(m6,LOW);
+  Serial.println("ARM UP");
+   digitalWrite(m3,HIGH); ///UP
+    digitalWrite(m4,LOW);
     delay(500);
-       digitalWrite(m5,LOW);
-    digitalWrite(m6,LOW);
+       digitalWrite(m3,LOW);
+    digitalWrite(m4,LOW);
 }
 
 void arm_down()
 {
-      digitalWrite(m5,LOW);//DOWN
-    digitalWrite(m6,HIGH);
+    Serial.println("ARM DOWN");
+      digitalWrite(m3,LOW);//DOWN
+    digitalWrite(m4,HIGH);
     delay(500);
-      digitalWrite(m5,LOW);
-    digitalWrite(m6,LOW);
+      digitalWrite(m3,LOW);
+    digitalWrite(m4,LOW);
 }
 
+void distance_val()
+{
+ digitalWrite(trigr,LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(trigr,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigr,LOW);
+  
+  duration=pulseIn(echo,HIGH);
+  distance=(duration*0.034)/2;
+  Serial.print("Distance= ");
+  Serial.println(distance);
+}
